483ea7d89dfb7faa8dea01986b11224951876f71
Manu Cornet <ecornet@twitter.com>
2022-02-07 15:21:00 +0000

    Little bit of reformatting/refactoring

--- background.js
+++ background.js
@@ -7,55 +7,48 @@ async function getCurrentTab() {
 async function copyDescriptionToClipboard() {
   let currentTab = await getCurrentTab();
   chrome.scripting.executeScript({
-    target: {tabId: currentTab.id},
-    func: function() {
+    target: { tabId: currentTab.id },
+    func: function () {
+      navigator.permissions
+        .query({ name: "clipboard-write" })
+        .then((result) => {
+          if (result.state === "granted" || result.state === "prompt") {
+            window.setTimeout(function () {
+              let tryHtml = false;
 
-      navigator.permissions.query({ name: 'clipboard-write' }).then(result => {
-        if (result.state === 'granted' || result.state === 'prompt') {
-          window.setTimeout(function() {
-            let tryHtml = false;
+              const title = document.querySelector(
+                ".phui-header-header"
+              ).textContent;
+              const anchor =
+                '<a href="' + document.location.href + '">' + title + "</a>";
+              let clipboardItemInput, data;
+              console.log('Created markup ' + anchor);
+              document.querySelector(".phui-header-header").focus();
 
-            const title = document.querySelector('.phui-header-header').textContent;
-            const anchor = '<a href="' + document.location.href + '">' + title + '</a>';
-            let blob, data;
-            if (tryHtml) {
-              blob = new Blob([anchor], { 'type': 'text/html;charset=utf-8' });
-              data = [new ClipboardItem({ 'text/html': blob })];
-            } else {
-              blob = new Blob([anchor], { 'type': 'text/plain;charset=utf-8' });
-              data = [new ClipboardItem({ 'text/plain': blob })];
-            }
-
-            document.querySelector('.phui-header-header').focus();
-            navigator.clipboard.write(data).then(
-              function() {
-                console.log('Copied ' + title);
+              try {
                 if (tryHtml) {
-                  navigator.clipboard.read({type: 'text/html'}).then((data) => {
-                    for (let i = 0; i < data.length; i++) {
-                      data[i].getType('text/html').then((blob) => {
-                        console.log(blob);
-                        const resp = new Response(blob);
-                        console.log(resp);
-                        resp.text().then(function(t) { console.log(t); });
-                      });
-                    }
-                  })
-                }
-              }, function(err) {
-                const errString = '' + err;
-                if (errString.includes('NotAllowedError') && errString.includes('not focused')) {
-                  alert('Please click anywhere on the page to focus it first, then try again.');
+                  const blob = new Blob([anchor], { type: "text/html" });
+                  clipboardItemInput = new ClipboardItem({"text/html": blob});
                 } else {
-                  alert('Unknown error: ' + err);
+                  const blob = new Blob([anchor], { type: "text/plain;charset=utf-8" });
+                  clipboardItemInput = new ClipboardItem({"text/plain": blob});
                 }
-              });
-          }, 1000);
-        } else {
-          alert('Could not get permission to write to the clipboard.');
-        }
-      });
-    }});
+                navigator.clipboard.write([clipboardItemInput]).then(() => {
+                  console.log("Wrote to clipboard");
+                  console.log(clipboardItemInput);
+                }).catch((e) => {
+                  console.log('Exception', e);
+                });
+              } catch (e) {
+                console.log('Also exception', e);
+              }
+            }, 1000);
+          } else {
+            alert("Could not get permission to write to the clipboard.");
+          }
+        });
+    },
+  });
 }
 
 chrome.action.onClicked.addListener(copyDescriptionToClipboard);
